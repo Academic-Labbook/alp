4892dbc06b1fb864b8b0197d0c1d11fa5a9a80ad
Sean Leavey <gpg@attackllama.com>
2018-03-04 18:37:22 +0000

    Update setting description

--- ssl-alp/admin/partials/post/author-settings-display.php
+++ ssl-alp/admin/partials/post/author-settings-display.php
@@ -1,6 +1,6 @@
 <p>
     <label for="ssl_alp_multiple_authors_checkbox">
         <input type="checkbox" name="ssl_alp_multiple_authors" id="ssl_alp_multiple_authors_checkbox" value="1" <?php checked( get_option( 'ssl_alp_multiple_authors' ) ); ?> />
-        <?php _e('Allow multiple authors to be specified for a single post', 'ssl-alp'); ?>
+        <?php _e('Allow multiple authors to be specified for posts', 'ssl-alp'); ?>
     </label>
 </p>
