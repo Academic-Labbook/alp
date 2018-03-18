403806c00aa742cfb34e9a72cd03325671a6098e
Sean Leavey <gpg@attackllama.com>
2018-03-18 20:31:47 +0100

    Add status icon support for single pages

--- ssl-alp/themes/alpine/content-single.php
+++ ssl-alp/themes/alpine/content-single.php
@@ -9,7 +9,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+		<?php
+		the_title(
+			sprintf(
+				'<h2 class="entry-title"><a href="%2$s" class="%1$s" rel="bookmark" >',
+				( 'status' === get_post_format() ) ? "status-post-title" : "", // add icon class for status updates
+				esc_url( get_permalink() )
+			),
+			'</a></h2>'
+		);
+		?>
 
 		<div class="entry-meta">
 			<?php ssl_alpine_the_post_meta(); ?>
