95f7b9fa8f8d2d707d3b89a5118ab0d16542569d
Sean Leavey <gpg@attackllama.com>
2018-03-18 22:19:13 +0100

    Remove unnecessary translation

--- ssl-alp/includes/class-wiki.php
+++ ssl-alp/includes/class-wiki.php
@@ -349,7 +349,7 @@ class SSL_ALP_Page_Contents extends WP_Widget {
 
 		?>
 		<p>
-			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'ssl-alp' ); ?></label>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
 		</p>
 		<?php
