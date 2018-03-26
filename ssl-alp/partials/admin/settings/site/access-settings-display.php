9142f23fd11e9c9c3eeb42916f3b2a3b0b57470f
Sean Leavey <gpg@attackllama.com>
2018-03-26 19:38:41 +0200

    Add warning to settings page about direct media access

--- ssl-alp/partials/admin/settings/site/access-settings-display.php
+++ ssl-alp/partials/admin/settings/site/access-settings-display.php
@@ -2,6 +2,6 @@
     <label for="ssl_alp_require_login_checkbox">
         <input type="checkbox" name="ssl_alp_require_login" id="ssl_alp_require_login_checkbox" value="1" <?php checked( get_option( 'ssl_alp_require_login' ) ); ?> />
         <?php _e('Require login to access site', 'ssl-alp'); ?>
-        <p class="description"><?php _e( 'You may wish to <a href="tools.php?page=ssl-alp-admin-tools">optimise private labbook settings</a> after enabling this.', 'ssl-alp' ); ?></p>
+        <p class="description"><?php _e( 'Note that this will <strong>not</strong> prevent direct access to media (images, documents, videos, etc.) uploaded to the site for those knowing the URL. You may wish to <a href="tools.php?page=ssl-alp-admin-tools">optimise private labbook settings</a> after enabling this.', 'ssl-alp' ); ?></p>
     </label>
 </p>
