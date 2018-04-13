03b0e269fe97001327ef0b46ff8e35e45d82d2fc
Sean Leavey <gpg@attackllama.com>
2018-04-13 16:04:10 +0200

    Remove unused post type flush

--- ssl-alp/includes/class-activator.php
+++ ssl-alp/includes/class-activator.php
@@ -8,10 +8,6 @@ class SSL_ALP_Activator {
 	 * Activate plugin.
 	 */
 	public static function activate() {
-		global $alp;
-
-		// flush rewrite rules for wiki
-		$alp->wiki->register_wiki_post_type();
-        flush_rewrite_rules();
+        
 	}
 }
--- ssl-alp/includes/class-deactivator.php
+++ ssl-alp/includes/class-deactivator.php
@@ -8,10 +8,6 @@ class SSL_ALP_Deactivator {
 	 * Deactivate plugin.
 	 */
 	public static function deactivate() {
-		global $alp;
-
-		// flush rewrite rules for wiki
-		$alp->wiki->register_wiki_post_type();
-		flush_rewrite_rules();
+		
 	}
 }
