cff91e682d289b13f1c2275dff17bdba24da0a81
Sean Leavey <gpg@attackllama.com>
2018-04-28 10:59:59 +0200

    Add note to run tool after enabling setting

--- ssl-alp/partials/admin/settings/post/reference-settings-display.php
+++ ssl-alp/partials/admin/settings/post/reference-settings-display.php
@@ -2,7 +2,7 @@
     <input type="checkbox" name="ssl_alp_enable_crossreferences" id="ssl_alp_enable_crossreferences_checkbox" value="1" <?php checked( get_option( 'ssl_alp_enable_crossreferences' ) ); ?> />
     <?php _e( 'Enable cross-references', 'ssl-alp' ); ?>
 </label>
-<p class="description"><?php _e( 'When enabled, a box is displayed containing links to posts and pages referenced from the current post or page, or by another post or page.', 'ssl-alp' ); ?></p>
+<p class="description"><?php _e( 'When enabled, a box is displayed containing links to posts and pages referenced from the current post or page, or by another post or page. You may wish to <a href="tools.php?page=ssl-alp-admin-tools">rebuild cross-references</a> after enabling this.', 'ssl-alp' ); ?></p>
 <br/>
 <p><?php _e( 'Enable shortcodes for:' ); ?></p>
 <label for="ssl_alp_doi_shortcode_checkbox">
