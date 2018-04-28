4b9208c0ec801ed9cf448610159d8442d8546e25
Sean Leavey <gpg@attackllama.com>
2018-04-28 11:02:52 +0200

    Tweak setting description

--- ssl-alp/partials/admin/settings/post/reference-settings-display.php
+++ ssl-alp/partials/admin/settings/post/reference-settings-display.php
@@ -9,7 +9,7 @@
     <input name="ssl_alp_doi_shortcode" type="checkbox" id="ssl_alp_doi_shortcode_checkbox" value="1" <?php checked( get_option( 'ssl_alp_doi_shortcode' ) ); ?> />
     <?php _e( 'Digital object identifiers (DOIs)' ); ?>
 </label>
-<p class="description"><?php _e( 'When enabled, persistent URLs to online information such as journal articles can be added to posts by enclosing their <a href="https://en.wikipedia.org/wiki/Digital_object_identifier">digital object identifiers (DOIs)</a> in <code>[doi]...[/doi]</code> tags', 'ssl-alp' ); ?>.</p>
+<p class="description"><?php _e( 'When enabled, persistent URLs to journal articles can be added to posts by enclosing their <a href="https://en.wikipedia.org/wiki/Digital_object_identifier">digital object identifiers (DOIs)</a> in <code>[doi]...[/doi]</code> tags', 'ssl-alp' ); ?>.</p>
 <label for="ssl_alp_arxiv_shortcode_checkbox">
     <input name="ssl_alp_arxiv_shortcode" type="checkbox" id="ssl_alp_arxiv_shortcode_checkbox" value="1" <?php checked( get_option( 'ssl_alp_doi_shortcode' ) ); ?> />
     <?php _e( 'ArXiv documents' ); ?>
