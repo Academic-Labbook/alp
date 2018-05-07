982577b9529fd587aece456e24130de207fbf564
Sean Leavey <gpg@attackllama.com>
2018-05-07 14:04:33 +0100

    Small footer tweak

--- ssl-alp/themes/alpine/footer.php
+++ ssl-alp/themes/alpine/footer.php
@@ -11,10 +11,7 @@
     </div> <!-- .row -->
 	</div><!-- #content -->
 
-	<?php do_action( 'ssl_alp_action_before_footer' ); ?>
-
 	<footer id="colophon" class="site-footer container" role="contentinfo">
-
 		<?php
 		$footer_nav = wp_nav_menu( array(
 			'theme_location'  => 'footer',
@@ -29,36 +26,21 @@
 		);
 		?>
 		<?php if ( ! empty( $footer_nav ) ) : ?>
-			<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'ssl-alp' ); ?>">
+		<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Menu', 'ssl-alp' ); ?>">
 			<?php echo $footer_nav; ?>
-			</nav>
+		</nav>
 		<?php endif ?>
-
-	<?php
-	$copyright_text = ssl_alp_get_option( 'copyright_text' );
-	?>
-	<?php if ( ! empty( $copyright_text ) ) : ?>
-
+		<?php if ( ! empty( ssl_alp_get_option( 'copyright_text' ) ) ) : ?>
 		<div id="copyright-wrap">
-			<div class="copyright-text"><?php echo wp_kses_post( $copyright_text ); ?></div>
+			<div class="copyright-text"><?php echo wp_kses_post( ssl_alp_get_option( 'copyright_text' ) ); ?></div>
 		</div>
-
-	<?php endif ?>
-
-	<?php
-		$powered_by = ssl_alp_get_option( 'powered_by' );
-	?>
-
-	<?php if ( true === $powered_by ) : ?>
-
-  		<div class="site-info" id="powered-by-wrap">
-  			<a href="<?php echo esc_url( __( 'https://alp.attackllama.com/', 'ssl-alp' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'ssl-alp' ), 'Academic Labbook Plugin for WordPress' ); ?></a>
-  		</div><!-- .site-info -->
-
-	<?php endif ?>
-
+		<?php endif ?>
+		<?php if ( true === ssl_alp_get_option( 'powered_by' ) ) : ?>
+		<div class="site-info" id="powered-by-wrap">
+			<a href="<?php echo esc_url( __( 'https://alp.attackllama.com/', 'ssl-alp' ) ); ?>"><?php printf( esc_html__( 'Powered by %s', 'ssl-alp' ), 'Academic Labbook Plugin for WordPress' ); ?></a>
+		</div><!-- .site-info -->
+		<?php endif ?>
 	</footer><!-- #colophon -->
-	<?php do_action( 'ssl_alp_action_after_footer' ); ?>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
