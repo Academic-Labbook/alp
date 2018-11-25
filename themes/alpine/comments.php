784c887f1e1feb95931363b410b30e231a37dac9
Sean Leavey <gpg@attackllama.com>
2018-11-25 20:39:07 +0100

    Update comment count text

--- themes/alpine/comments.php
+++ themes/alpine/comments.php
@@ -33,7 +33,7 @@ if ( post_password_required() ) {
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s comments on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'ssl-alp' ) ),
+					esc_html( _nx( '%1$s comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'ssl-alp' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
