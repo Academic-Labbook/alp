d043eacc728d61e6820e4c7ea0398ceb967b65b7
Sean <sean@attackllama.com>
2018-12-30 19:21:22 +0000

    Update text domain

--- themes/alpine/comments.php
+++ themes/alpine/comments.php
@@ -63,7 +63,7 @@ if ( post_password_required() ) {
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() ) :
 			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'alpine' ); ?></p>
+			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'ssl-alpine' ); ?></p>
 			<?php
 		endif;
 
