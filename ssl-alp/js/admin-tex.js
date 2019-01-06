a2cdee7061cf561d136871902d6c2816a5fcf073
Sean Leavey <gpg@attackllama.com>
2019-01-07 00:04:53 +0100

    Fix tex text field script

--- ssl-alp/js/admin-tex.js
+++ ssl-alp/js/admin-tex.js
@@ -2,15 +2,19 @@
     'use strict';
 
     var $checkbox = $( "#ssl_alp_tex_use_custom_urls_checkbox" );
-    var $js_textbox = $( "#ssl_alp_katex_js_url_textbox" );
-    var $css_textbox = $( "#ssl_alp_katex_css_url_textbox" );
+    var $katex_js_textbox = $( "#ssl_alp_katex_js_url_textbox" );
+    var $katex_copy_js_textbox = $( "#ssl_alp_katex_copy_js_url_textbox" );
+    var $katex_css_textbox = $( "#ssl_alp_katex_css_url_textbox" );
+    var $katex_copy_css_textbox = $( "#ssl_alp_katex_copy_css_url_textbox" );
 
     function toggle_textboxes( state ) {
         // readonly state
         var readonly = state ? '' : 'readonly';
 
-        $js_textbox.prop( "readonly", readonly );
-        $css_textbox.prop( "readonly", readonly );
+        $katex_js_textbox.prop( "readonly", readonly );
+        $katex_copy_js_textbox.prop( "readonly", readonly );
+        $katex_css_textbox.prop( "readonly", readonly );
+        $katex_copy_css_textbox.prop( "readonly", readonly );
     }
 
     // initial disabled state
